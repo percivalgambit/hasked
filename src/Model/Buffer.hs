4e1ecb24e0966d62ad8fa3e9bb3c1ded20727bfb
Lee Ehudin <percivalgambit@gmail.com>
2015-05-21 17:50:49 -0500

    began work on functions for interacting with the buffer

--- src/Model/Buffer.hs
+++ src/Model/Buffer.hs
@@ -1,8 +1,26 @@
 module Model.Buffer where
 
-import Data.ListLike.Zipper
+import Data.ListLike.Zipper as Z
 
 import Data.IORef
 import Data.Sequence
 
-type Buffer = IORef (Zipper (Seq Char))
+type Buffer       = Zipper (Seq Char)
+type MBuffer      = IORef Buffer
+type ModifyBuffer = MBuffer -> IO ()
+
+newBuffer :: IO MBuffer
+newBuffer = newIORef Z.empty
+
+getBuffer :: MBuffer -> IO Buffer
+getBuffer = readIORef
+
+bLeft, bRight :: ModifyBuffer
+bLeft  = flip modifyIORef' Z.left
+bRight = flip modifyIORef' Z.right
+
+bInsert :: Char -> ModifyBuffer
+bInsert c = flip modifyIORef' (Z.insert c)
+
+bDelete :: ModifyBuffer
+bDelete = flip modifyIORef' Z.delete
