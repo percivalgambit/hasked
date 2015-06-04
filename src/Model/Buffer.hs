f6673777a976fba22ddaf145e64fce1b5d67ba7c
Lee Ehudin <percivalgambit@gmail.com>
2015-06-04 17:49:58 -0500

    Added lens definitions for the rest of the buffer functions

--- src/Model/Buffer.hs
+++ src/Model/Buffer.hs
@@ -1,20 +1,29 @@
+{-# LANGUAGE Rank2Types #-}
+
 module Model.Buffer (newBuffer, newBufferFromFile, writeBufferToFile,
                      left, right, upLine, downLine, insert, delete) where
 
 import qualified Data.ListLike.Zipper as Z
 
 import Control.Category ((>>>))
-import Control.Lens.Lens ((<&>))
+import Control.Lens.Lens ((&), (<&>), Lens')
 import Data.IORef (newIORef, readIORef, IORef)
 import Data.ListLike.Instances ()
 import qualified Data.ListLike.IO as LLIO
 import qualified Data.ListLike.String as LLS
 import qualified Data.Sequence as S
 
-type Line    = S.Seq Char
-type Lines   = S.Seq Line
-type Buffer  = Z.Zipper Lines
-type MBuffer = IORef Buffer
+type Line        = S.Seq Char
+type FocusedLine = Z.Zipper Line
+type Lines       = S.Seq Line
+type Buffer      = Z.Zipper Lines
+type MBuffer     = IORef Buffer
+
+focusLine :: Lens' Buffer FocusedLine
+-- focusLine :: Functor f => (FocusedLine -> f FocusedLine) -> Buffer -> f Buffer
+focusLine focus_fn buffer = focus_fn focus <&> unfocusLine where
+    unfocusLine focus' = Z.unzip focus' & flip Z.replace buffer
+    focus              = Z.cursor buffer & Z.zip
 
 newBuffer :: IO MBuffer
 newBuffer = newIORef Z.empty
@@ -29,14 +38,17 @@ writeBufferToFile filepath buffer = readIORef buffer
                                     <&> (Z.unzip >>> LLS.unlines)
                                     >>= LLIO.writeFile filepath
 
-left, right, upLine, downLine :: MBuffer -> IO ()
-left = undefined
-right = undefined
-upLine = undefined
-downLine = undefined
+left, right, upLine, downLine :: Lens' Buffer FocusedLine
+-- :: Functor f => (FocusedLine -> f FocusedLine) -> Buffer -> f Buffer
+left focus_fn = focusLine (focus_fn >>> fmap Z.left)
+right focus_fn = focusLine (focus_fn >>> fmap Z.right)
+upLine focus_fn = focusLine focus_fn . Z.left
+downLine focus_fn = focusLine focus_fn . Z.right
 
-insert :: Char -> MBuffer -> IO ()
-insert = undefined
+insert :: Char -> Lens' Buffer FocusedLine
+-- insert :: Functor f => (FocusedLine -> f FocusedLine) -> Buffer -> f Buffer
+insert c focus_fn = focusLine (focus_fn <$> Z.insert c)
 
-delete :: MBuffer -> IO ()
-delete = undefined
+delete :: Lens' Buffer FocusedLine
+-- delete :: Functor f => (FocusedLine -> f FocusedLine) -> Buffer -> f Buffer
+delete focus_fn = focusLine (focus_fn >>> fmap Z.delete)
